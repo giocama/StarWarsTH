4e3e35d2853e5720b77e3915d61db6b9cc00d2b3
Giovanni Calleja <giocallejam@gmail.com>
2022-03-25 10:30:52 -0600

    nuevo repositorio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Navbar from './Componentes/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Container, Dimmer, Loader } from 'semantic-ui-react';
+import Home from './Componentes/Home';
+import People from './Componentes/People';
+import Favorite from './Componentes/Favorite';
+
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [favorite, setFavorite] = useState([]);
+  const [loanding, setLoanding] = useState(true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.result);
+      setLoanding(false);
+    }
+
+    async function fetchFavorite() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setFavorite(data.result);
+      setLoanding(false);
+    }
+
+      fetchPeople();
+      fetchFavorite();
+      
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Container>
+          {loanding ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ) : (
+            <Switch>
+              <Route exact path='/'> <Home/> </Route>
+              <Route exact path='/people'> <People data={people} /> </Route>
+              <Route exact path='/favorite'> <Fav data={Favorite} /> </Route>
+            </Switch>
+          )}
+          
+        </Container>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Componentes/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Menu, Container } from 'semantic-ui-css'
+import { Link } from 'react-router-dom'
+
+export default function Navbar () {
+    return (
+        <Menu inverted>
+            <Container>
+                <Link to='/'>
+                    <Menu.Item name='Star Wars Api' />                
+                </Link>
+                <Link to='/people'>
+                    <Menu.Item name='People' />                
+                </Link>
+                <Link to='/favoririte'>
+                    <Menu.Item name='Favorite' />
+                </Link>
+                
+            </Container>
+        </Menu>
+    );
+}
